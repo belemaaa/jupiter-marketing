b0ad5138fde9c36b4662bb2bb3885b29c1f80ae6
belem <graceitamunoala@gmail.com>
2023-06-26 23:24:19 +0100

    styles 'what we do' component

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -6,15 +6,15 @@ const WhatWeDo = () => {
   return (
     <div className='flex flex-col mt-16 relative'>
         <div className='flex items-center justify-center'>
-            <p className='text-orange-700 text-4xl font-bold mb-4'>WHAT WE DO</p>
+            <p className='text-orange-700 text-4xl font-bold mb-4 border-b border-b-gray-700 pb-3'>WHAT WE DO</p>
         </div>
 
         <div className='flex flex-col items-center justify-center mx-8'>
-            <div className='flex flex-row pt-12 pb-5'>
+            <div className='flex flex-row pt-8 pb-3 mb-3'>
                 <div className=''>
-                    <img src={creativeSolutions} className='brightness-50 hover:scale-110 duration-500 mx-8 w-96 h-60'/>
-                    <div className='absolute top-20 left-36 bottom-0 flex items-center justify-center'>
-                        <a className=' text-2xl text-white border-b-4 border-white pb-3 font-semibold
+                    <img src={creativeSolutions} className='brightness-50 hover:scale-110 duration-500 mx-8 w-72 h-40'/>
+                    <div className='absolute -top-80 left-52 bottom-0 flex items-center justify-center'>
+                        <a className=' text-2xl text-white border-b-4 border-white font-semibold
                         flex flex-row'>
                             Digital Branding
                             <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
@@ -23,26 +23,102 @@ const WhatWeDo = () => {
                 </div>
 
                 <div>
-                    <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-96 h-60'/>
-                    <div className='absolute top-20 left-48 bottom-0 flex items-center justify-between'>
-                        <a className=' text-2xl text-white border-b-4 border-white pb-3 font-semibold
-                        flex flex-row ml-96'>
-                            Digital Branding
+                    <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                    <div className='absolute -top-80 left-44 bottom-0 flex items-center justify-between'>
+                        <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                        flex flex-row ml-96 pb-2'>
+                            Creative Solution
                             <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
                         </a>
                     </div>
                 </div>
 
                 <div>
-                    <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-96 h-60'/>
-                    <div className='absolute top-20 -right-24 left-96 bottom-0 flex items-center justify-center'>
-                        <a className=' text-2xl text-white border-b-4 border-white pb-3 font-semibold
-                        flex flex-row ml-96'>
-                            Digital Branding
+                    <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                    <div className='absolute -top-80 -right-24 left-60 bottom-0 flex items-center justify-center'>
+                        <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                        flex flex-row ml-96 pb-2'>
+                            Web Solution
                             <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
                         </a>
                     </div>
+                </div> 
+            </div>
+
+
+            <div className='flex flex-col items-center justify-center mx-8'>
+                <div className='flex flex-row pt-4 pb-5'>
+                    <div className=''>
+                        <img src={creativeSolutions} className='brightness-50 hover:scale-110 duration-500 mx-8 w-72 h-40'/>
+                        <div className='absolute top-24 left-52 bottom-0 flex items-center justify-center'>
+                            <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                            flex flex-row pb-2'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                        <div className='absolute top-24 left-44 bottom-0 flex items-center justify-between'>
+                            <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                            flex flex-row ml-96 pb-2'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                        <div className='absolute top-24 -right-24 left-60 bottom-0 flex items-center justify-center'>
+                            <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                            flex flex-row ml-96 pb-2'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            
+
+           <div className='flex flex-col items-center justify-center mx-8'>
+                <div className='flex flex-row pt-4 pb-5'>
+                    <div className=''>
+                        <img src={creativeSolutions} className='brightness-50 hover:scale-110 duration-500 mx-8 w-72 h-40'/>
+                        <div className='absolute top-96 left-52 bottom-0 flex items-center justify-center'>
+                            <a className=' text-2xl text-white border-b-4 border-white pb-2 font-semibold
+                            flex flex-row'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                        <div className='absolute top-96 left-44 bottom-0 flex items-center justify-between'>
+                            <a className=' text-2xl text-white border-b-4 border-white pb-2 font-semibold
+                            flex flex-row ml-96'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={creativeSolutions} className='brightness-50 mx-8 hover:scale-110 duration-500 w-72 h-40'/>
+                        <div className='absolute top-96 -right-24 left-60 bottom-0 flex items-center justify-center'>
+                            <a className=' text-2xl text-white border-b-4 border-white font-semibold
+                            flex flex-row ml-96 pb-2'>
+                                Digital Branding
+                                <BsArrowRightCircle size={20} className='text-white mt-2 ml-2 hover:rotate-90 duration-200'/>
+                            </a>
+                        </div>
+                    </div>
+                </div> 
             </div> 
         </div>
     </div>
