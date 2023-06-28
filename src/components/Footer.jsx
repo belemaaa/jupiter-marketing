970dd166aaac3a30089efaba82db886f66825e9d
belem <graceitamunoala@gmail.com>
2023-06-28 16:54:21 +0100

    setting footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-700 mx-16 flex flex-col'>
-        <div className='flex flex-row'>
-
+    <div className='bg-gray-700 flex flex-col'>
+        <div className='flex flex-row mx-24 text-white'>
+            <div className='flex flex-col'>
+                <p>Quick Links</p>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Project</li>
+                    <li>Team</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
         </div>
     </div>
   )
