a971ce6ca32125cc2c8bebb8f36ff2d3b950edd5
belem <graceitamunoala@gmail.com>
2023-07-01 15:22:41 +0100

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,7 +21,7 @@ const Projects = () => {
 
         <div className='flex flex-col bg-black bg-opacity-5 pt-6 pb-12'>
             <div className='mt-20'>
-                <p className='text-black text-3xl md:text-4xl font-bold mb-2 text-center'>
+                <p className='text-orange-700 text-3xl md:text-4xl font-bold mb-2 text-center'>
                     Our Completed Projects
                 </p>
                 <p className='text-gray-700 font-bold text-center'>
@@ -31,47 +31,65 @@ const Projects = () => {
 
             <div className='flex flex-row items-center justify-center my-10'>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={creativeProject1} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>Insurance</p>
+                    <a href=''>
+                        <img src={creativeProject1} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>INSURANCE</p>
+                    </a>
 
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={creativeProject2} className='shadow-xl rounded-2xl'/>
-                    <p className='mt-4 text-center text-lg text-gray-500'>Bee</p>
+                    <a href=''>
+                        <img src={creativeProject2} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>BEE</p>
+                    </a>
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={digitalProject2} className='shadow-xl rounded-2xl'/>
-                    <p className='mt-4 text-center text-lg text-gray-500'>Industrial Service</p>
+                    <a href=''>
+                        <img src={digitalProject2} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>INDUSTRIAL SERVICE</p>
+                    </a>
                 </div>
             </div>
 
             <div className='flex flex-row items-center justify-center my-10'>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={digitalProject1} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>Architect Digital Studio</p>
+                    <a href=''>
+                        <img src={digitalProject1} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>ARCHITECT DIGITAL STUDIO</p>
+                    </a>
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={ourProject1} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>S E O</p>
+                    <a href=''>
+                        <img src={ourProject1} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>S E O</p>
+                    </a>
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={ourProject2} className='shadow-xl rounded-2xl'/>
-                    <p className='mt-4 text-center text-lg text-gray-500'>Digital Create</p>
+                    <a href=''>
+                        <img src={ourProject2} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>DIGITAL CREATE</p>
+                    </a>
                 </div>
             </div>
 
             <div className='flex flex-row items-center justify-center my-10'>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={ourProject3} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>Content Creator</p>
+                    <a href=''>
+                        <img src={ourProject3} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>CONTENT CREATOR</p>
+                    </a>
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={webProject1} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>Selling.</p>
+                    <a href=''>
+                        <img src={webProject1} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>SELLING.</p>
+                    </a>
                 </div>
                 <div className='mx-8 w-96 hover:scale-110 duration-700'>
-                    <img src={webProject2} className='shadow-xl rounded-2xl'/>
-                    <p className='text-center text-gray-500 text-lg mt-4'>Relief</p>
+                    <a href=''>
+                        <img src={webProject2} className='shadow-xl rounded-2xl'/>
+                        <p className='text-center text-gray-500 font-bold text-lg mt-4'>RELIEF</p>
+                    </a>
                 </div>
             </div>
         </div>
