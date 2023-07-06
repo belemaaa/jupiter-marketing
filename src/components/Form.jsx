317a7837bf6f6372e0e3311e05894cada40f3058
belem <graceitamunoala@gmail.com>
2023-07-06 17:43:19 +0100

    added image to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import banner3 from '../assets/banner-images/banner3.jpg'
 
 const Form = () => {
     const form = useRef();
@@ -26,88 +27,95 @@ const Form = () => {
     }
 
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <div className='border border-gray-500 my-16 rounded-lg'>
-                <div className='px-12'>
-                    <div>
-                        <p className='mt-16 text-black font-semibold text-2xl text-center'>
-                            Want to Know More?? 
-                        </p>
-                        <p className=' text-black font-semibold text-2xl text-center'>
-                            Drop Us A Mail
-                        </p>
-                    </div>
-                    <div className='mt-6'>
-                        <form ref={form} onSubmit={sendEmail}>
-                            <label className='text-gray-700 font-semibold'>
-                                First Name:
-                            </label> 
-                            <br/>
-                            <input 
-                            type="text" 
-                            name="f_name"  
-                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
-                            required/>
-                            <br/>
+        <div className='flex flex-row'>
+            <div className='flex flex-col items-start justify-start'>
+                <div className='border border-gray-500 mt-20 mb-16 ml-24 rounded-lg'>
+                    <div className='px-12'>
+                        <div>
+                            <p className='mt-16 text-black font-semibold text-2xl text-center'>
+                                Want to Know More?? 
+                            </p>
+                            <p className=' text-black font-semibold text-2xl text-center'>
+                                Drop Us A Mail
+                            </p>
+                        </div>
+                        <div className='mt-6'>
+                            <form ref={form} onSubmit={sendEmail}>
+                                <label className='text-gray-700 font-semibold'>
+                                    First Name:
+                                </label> 
+                                <br/>
+                                <input 
+                                type="text" 
+                                name="f_name"  
+                                className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
+                                required/>
+                                <br/>
 
-                            <label className='text-gray-700 font-semibold'>
-                                Last Name:
-                            </label> 
-                            <br/>
-                            <input 
-                            type="text" 
-                            name="f_name"  
-                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
-                            required/>
-                            <br/>
+                                <label className='text-gray-700 font-semibold'>
+                                    Last Name:
+                                </label> 
+                                <br/>
+                                <input 
+                                type="text" 
+                                name="f_name"  
+                                className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
+                                required/>
+                                <br/>
 
-                            <label className='text-gray-700 font-semibold'>
-                                Email:
-                            </label> 
-                            <br/>
-                            <input 
-                            type="email" 
-                            name="email"  
-                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
-                            required/>
-                            <br/>
+                                <label className='text-gray-700 font-semibold'>
+                                    Email:
+                                </label> 
+                                <br/>
+                                <input 
+                                type="email" 
+                                name="email"  
+                                className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
+                                required/>
+                                <br/>
 
-                            <label className='text-gray-700 font-semibold'>
-                                Contact Number:
-                            </label> 
-                            <br/>
-                            <input 
-                            type="phone" 
-                            name="phone"  
-                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
-                            required/>
-                            <br/> 
+                                <label className='text-gray-700 font-semibold'>
+                                    Contact Number:
+                                </label> 
+                                <br/>
+                                <input 
+                                type="phone" 
+                                name="phone"  
+                                className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
+                                required/>
+                                <br/> 
 
-                            <label className='text-gray-700 font-semibold'>
-                                Leave A Message:
-                            </label> 
-                            <br/>
-                            <textarea
-                            name="message" 
-                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 h-44 text-black mb-6' 
-                            required/>
-                            <br/>
+                                <label className='text-gray-700 font-semibold'>
+                                    Leave A Message:
+                                </label> 
+                                <br/>
+                                <textarea
+                                name="message" 
+                                className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 h-44 text-black mb-6' 
+                                required/>
+                                <br/>
 
-                            <input 
-                            type="submit" 
-                            value="Send" 
-                            // onClick={sendBtn}
-                            className='flex mx-40 bg-gray-700 
-                            hover:bg-black text-white font-semibold py-2 px-4 mb-3'
-                            />
-                        </form>
+                                <input 
+                                type="submit" 
+                                value="Send" 
+                                // onClick={sendBtn}
+                                className='flex mx-40 bg-gray-700 
+                                hover:bg-black text-white font-semibold py-2 px-4 mb-3'
+                                />
+                            </form>
+                        </div>
+
                     </div>
 
                 </div>
-
-            </div>
           
+            </div>
+
+            <div className='mt-52 ml-20'>
+                <img src={banner3} className='w-5/6 h-3/4 rounded-full drop-shadow-2xl'/>
+            </div>
         </div>
+       
     )
 }
 
