5e25058fb1d4cb780e2aec9e20fc2e85b18cb278
belem <graceitamunoala@gmail.com>
2023-07-06 15:14:14 +0100

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,9 +28,9 @@ const Form = () => {
     }
     return (
         <div className='flex flex-col items-center justify-center'>
-            <div className='border border-gray-500 mt-16 rounded-lg'>
+            <div className='border border-gray-500 my-16 rounded-lg'>
                 <div className='px-12'>
-                    <div className=''>
+                    <div>
                         <p className='mt-16 text-black font-semibold text-2xl text-center'>
                             Want to Know More?? 
                         </p>
@@ -40,7 +40,10 @@ const Form = () => {
                     </div>
                     <div className='mt-6'>
                         <form ref={form} onSubmit={sendEmail}>
-                            <label className='text-gray-700 font-semibold'>First Name:</label> <br/>
+                            <label className='text-gray-700 font-semibold'>
+                                First Name:
+                            </label> 
+                            <br/>
                             <input 
                             type="text" 
                             name="f_name"  
@@ -48,7 +51,10 @@ const Form = () => {
                             required/>
                             <br/>
 
-                            <label className='text-gray-700 font-semibold'>Last Name:</label> <br/>
+                            <label className='text-gray-700 font-semibold'>
+                                Last Name:
+                            </label> 
+                            <br/>
                             <input 
                             type="text" 
                             name="f_name"  
@@ -56,7 +62,10 @@ const Form = () => {
                             required/>
                             <br/>
 
-                            <label className='text-gray-700 font-semibold'>Email:</label> <br/>
+                            <label className='text-gray-700 font-semibold'>
+                                Email:
+                                </label> 
+                            <br/>
                             <input 
                             type="email" 
                             name="email"  
@@ -64,17 +73,31 @@ const Form = () => {
                             required/>
                             <br/>
 
-                            <label className='text-gray-700 font-semibold'>Contact Number:</label> <br/>
+                            <label className='text-gray-700 font-semibold'>
+                                Contact Number:
+                                </label> 
+                            <br/>
                             <input 
                             type="phone" 
                             name="phone"  
                             className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 text-black mb-6' 
                             required/>
                             <br/> 
-                            <label>Message</label>
-                            <textarea name="message" required/>
+
+                            <label className='text-gray-700 font-semibold'>
+                                Leave A Message:
+                            </label> 
                             <br/>
-                            <input type="submit" value="Send" onClick={sendBtn} />
+                            <textarea
+                            name="message" 
+                            className='border border-black pl-5 placeholder:font-bold rounded-sm py-2 w-96 h-44 text-black mb-6' 
+                            required/>
+                            <br/>
+
+                            <input 
+                            type="submit" 
+                            value="Send" 
+                            onClick={sendBtn} />
                         </form>
                     </div>
 
