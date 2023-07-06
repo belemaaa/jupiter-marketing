7e96f7584076dcf2f9bfe15d005d3fecd827783f
belem <graceitamunoala@gmail.com>
2023-07-06 13:05:17 +0100

    installing emailjs

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Form = () => {
+  return (
+    <div>
+        <div>
+            Want to Know More?? Drop Us A Mail
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Form
