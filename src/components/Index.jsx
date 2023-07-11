ace41baed6924c927acbe36d1ac2ce3dbf480aea
belem <graceitamunoala@gmail.com>
2023-07-11 10:26:03 +0100

    commit

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -338,39 +338,39 @@ const Index = () => {
             </div>
           </div>
 
-          <div className='bg-gray-700 flex flex-col pb-4 w-screen overflow-hidden'>
-            <div className='flex flex-col md:flex-row ml-8 md:mx-24 text-white pt-12 pb-8'>
+          <div className='flex flex-col'>
+            <div className='flex flex-col md:flex-row ml-8 md:mx-24 text-black pt-12 pb-8'>
                 <div className='flex flex-col mr-8'>
                     <p className='text-2xl pb-2'>Quick Links</p>
-                    <div className='border border-gray-500 w-60'></div>
-                    <ul className='mt-4 text-gray-300'>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                    <div className='border border-gray-300 w-60'></div>
+                    <ul className='mt-4 text-blue-500'>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Home
                             </a>
                         </li>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 About Us
                             </a>
                         </li>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Project
                             </a>
                         </li>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Team
                             </a>
                         </li>
                         <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Contact Us
                             </a>
                         </li>
@@ -379,23 +379,23 @@ const Index = () => {
 
                 <div className='flex flex-col md:mx-8 my-6 md:my-0'>
                     <p className='text-2xl pb-2'>Our Services</p>
-                    <div className='border border-gray-500 w-60'></div>
-                    <ul className='mt-4 text-gray-300'>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                    <div className='border border-gray-300 w-60'></div>
+                    <ul className='mt-4 text-blue-500'>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Digital Branding
                             </a>
                         </li>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Creative Solution
                             </a>
                         </li>
-                        <li className='pb-2'>
-                            <a className='flex flex-row'>
-                                <RxDotFilled size={20} className='text-white mr-3 mt-0.5'/>
+                        <li className=''>
+                            <a className='flex flex-row underline'>
+                                <RxDotFilled size={20} className='text-black mr-3 mt-0.5'/>
                                 Web Solution
                             </a>
                         </li>
@@ -466,8 +466,8 @@ const Index = () => {
                         Site Map
                     </Link>
                 </div>
-          </div>    
-        </div>
+            </div>    
+          </div>
 
           
           
